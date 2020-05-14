d2f6bcf9f0f9dde8cb891aa894c93762755f0eeb
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-05-14 18:54:48 -0300

    envio de email de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'acdbd637e6d907',
+    pass: '6ceae2fbbec777',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

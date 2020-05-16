b1e1933c8d207a665592e54fb63c2473464282d7
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-05-16 17:01:38 -0300

    tratamento de exceção e variáveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'acdbd637e6d907',
-    pass: '6ceae2fbbec777',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
